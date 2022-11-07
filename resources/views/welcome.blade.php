ff667fbd12bb045ab0cd076d490b3f1739fe73f6
github <github>
2022-11-07 18:53:48 +0900

    ログイン機能追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
+            <h1>Welcome to the Microposts2</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', '今すぐ会員登録!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
